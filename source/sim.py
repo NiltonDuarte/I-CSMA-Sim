ddf36d1f4ad3c70d0de2063061b8d456dd355124
NiltonDuarte <nilton.gduarte@gmail.com>
2017-07-10 11:56:17 -0300

    output formatting

--- source/sim.py
+++ source/sim.py
@@ -13,18 +13,22 @@ lattice = Lattice(size,distance)
 interfGraphLattice = InterferenceGraph(lattice, 5.1)
 
 
-windowSize = 20
+windowP1 = 20
+windowP2 = 8
 rho = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 beta = float(sys.argv[1])
 testesIt = 1000000
 rounds = 30
-f = open('results'+sys.argv[1], 'w')
+f = open('results_beta'+sys.argv[1], 'w')
+header = "Rho, Beta, QMean, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16\n"
+f.write(str(header))
+f.flush()
 
 for i in range(30):
 	for r in rho:
 		interfGraphLattice = InterferenceGraph(lattice, 5.1)
-		icsma = I_CSMA(interfGraphLattice, beta, windowSize, windowSize, r, 0.5)	
-		print "I-CSMA Window = ", windowSize, " Rho = ",r, " beta = ", beta, " it = ", i
+		icsma = I_CSMA(interfGraphLattice, beta, windowP1, windowP2, r, 0.5)	
+		print "I-CSMA Window = ", (windowP1, windowP2), " Rho = ",r, " beta = ", beta, " it = ", i
 		schedule = icsma.run(testesIt)
 		queue=0
 		queuesList = []
@@ -32,7 +36,7 @@ for i in range(30):
 		for node in icsma.interfGraph.nodes:
 			queuesList.append(node.queueSize)
 			queue += node.queueSize
-		results=[r , beta, round(queue/16.0,2), queuesList]
+		results=", ".join(str(x) for x in ([r , beta, round(queue/16.0,2)] + queuesList))
 		f.write(str(results))
 		f.write('\n')
 		f.flush()
