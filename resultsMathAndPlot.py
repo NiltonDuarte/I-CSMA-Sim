afd94318f4df25421d908c687d7b33918ca1da07
NiltonDuarte <nilton.gduarte@gmail.com>
2017-11-15 02:11:04 -0200

    more math on results

--- resultsMathAndPlot.py
+++ resultsMathAndPlot.py
@@ -22,7 +22,6 @@ print floatIdxs
 with open('./resultados/gitignoreR_All.csv', 'r') as csvFile:
   #skip header
   #next(csvFile)
-
   reader = csv.reader(csvFile, delimiter=',')
   for row in reader:
     #convert string to float
@@ -59,14 +58,14 @@ print graphL
 #algo ['CFv2', 'CFv2-NoQ', 'CFv2NQF', 'CFv4', 'CFv4-NoQ', 'CFv4NQF', 'HICSMA', 'HICSMA-NCP2', 'HICSMASEC', 'HICSMASEC-NCP2', 'HICSMASECNQF', 'ICSMA']
 
 
-QueueMean = collections.namedtuple("QueueMean", "meanL, rho, beta, algo, graph, resultsL")
-queueMeanL = []
+QueueInfo = collections.namedtuple("QueueInfo", "mean, queueML, rho, beta, algo, graph, resultsL")
+queueMeanGraphL = []
 for graph in graphL:
   for rho in rhoL:
     for beta in betaL:
       for algo in algoL:
-        queue = QueueMean([], rho, beta, algo, graph, [])
-        queueMeanL.append(queue)
+        queue = QueueInfo([-1], [], rho, beta, algo, graph, [])
+        queueMeanGraphL.append(queue)
 
 lenGraph = len(graphL)
 lenRho = len(rhoL)
@@ -81,40 +80,106 @@ def getIdx(rho, beta, algo, graph):
   algoIdx = algoL.index(algo)
   return gIdx*lenRho*lenBeta*lenAlgo + rIdx*lenBeta*lenAlgo + bIdx*lenAlgo + algoIdx
 
-"""
-#getidx func test
+print "Test 1"
 for graph in graphL:
   for rho in rhoL:
     for beta in betaL:
       for algo in algoL:
-        t = queueMeanL[getIdx(rho=rho, beta=beta, algo=algo, graph=graph)]
+        t = queueMeanGraphL[getIdx(rho=rho, beta=beta, algo=algo, graph=graph)]
         if t.rho != rho or t.beta != beta or t.algo != algo or t.graph != graph:
           print "Error"
-"""
-
+print "Done\n"          
 
 
 for resultRow in doc:
   param = resultRow.rho, resultRow.beta, resultRow.algo, resultRow.graph
-  qMean = queueMeanL[getIdx(*param)]
+  qMean = queueMeanGraphL[getIdx(*param)]
   mean = resultRow.mean
-  if len(qMean.meanL) < 5:
-    qMean.meanL.append(mean)
+  if len(qMean.queueML) < 5:
+    qMean.queueML.append(mean)
   qMean.resultsL.append(resultRow)
 
-#checks for wrong duplicate dealing
-for q in queueMeanL:
-  if len(q.resultsL) > 5:
-    #print len(q.meanL), q.rho, q.beta, q.algo, "   \t" if q.algo in ["CFv2", "CFv4"] else "\t", q.graph
-    for result in q.resultsL:
-      if not result.mean in q.meanL:
-        print "Error",result.mean, q.resultsL, q.meanL
 
-#checks for forgot sims
-sum = 0
+print "Checking for wrong duplicates dealing"
+for q in queueMeanGraphL:
+  print ".\r",
+  if len(q.resultsL) > 5:
+    #print len(q.queueML), q.rho, q.beta, q.algo, "   \t" if q.algo in ["CFv2", "CFv4"] else "\t", q.graph
+    for result in q.resultsL:
+      if not result.mean in q.queueML:
+        print "Error",result.mean, q.resultsL, q.queueML
+print "\nDone\n"
+
+removeList = []
+print "Checks for forgotten sims"
+for q in queueMeanGraphL:
+  if len(q.queueML) != 5:# and q.rho==0.5:
+    print "Error",
+    print len(q.queueML), q.rho, q.beta, q.algo, "   \t" if q.algo in ["CFv2", "CFv4"] else "\t", q.graph, "\r",
+    removeList.append(q)
+#for q in removeList:
+#  queueMeanGraphL.remove(q)
+print "\nDone\n"
+
+print "Calculating Means"
+for q in queueMeanGraphL:
+  print ".\r",
+  summ = sum(q.queueML)
+  if summ>0:
+    mean = summ/len(q.queueML)
+    q.mean[0] = mean
+print "\nDone\n"
+
+#data structure
+queueMeanL = []
+for rho in rhoL:
+  for beta in betaL:
+    for algo in algoL:
+      queue = QueueInfo([-1], [], rho, beta, algo, None, None)
+      queueMeanL.append(queue)
+
+print "Test 2"
+for rho in rhoL:
+  for beta in betaL:
+    for algo in algoL:
+      t = queueMeanL[getIdx(rho=rho, beta=beta, algo=algo, graph=graphL[0])]
+      if t.rho != rho or t.beta != beta or t.algo != algo:
+        print "Error"
+print "Done\n"
+
+#mean over all graphs of a given rho, beta and algo
+for qm in queueMeanL:
+  for graph in graphL:
+    param = qm.rho, qm.beta, qm.algo, graph
+    qMean = queueMeanGraphL[getIdx(*param)]
+    qm.queueML.append(qMean.mean[0])
+
+print "Calculating Means"
 for q in queueMeanL:
-  
-  if len(q.meanL) != 5:# and q.rho==0.5:
-    print len(q.meanL), q.rho, q.beta, q.algo, "   \t" if q.algo in ["CFv2", "CFv4"] else "\t", q.graph
-    sum += 1
-print sum
+  print ".\r",
+  summ = sum(q.queueML)
+  if summ>0:
+    mean = summ/len(q.queueML)
+    q.mean[0] = mean
+print "\nDone\n"
+
+
+#getting the best beta fit for each algo
+queueMeanL.sort(key=lambda obj: obj.mean[0])
+bestBetaQueues = []
+for qm in queueMeanL:
+  inBB = False
+  for bb in bestBetaQueues:
+    if qm.algo == bb.algo and qm.rho == bb.rho:
+      if bb.mean > qm.mean:
+        print "Error"
+      inBB = True
+      break
+  if not inBB:
+    bestBetaQueues.append(qm)
+
+
+bestBetaQueues.sort(key=lambda obj: obj.rho)
+for q in bestBetaQueues:
+  #if q.rho == 0.8:
+  print "{}\t{}\t{}\t{}".format(q.rho, q.beta, q.algo, round(q.mean[0],3))
